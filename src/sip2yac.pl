7cd353e12a7da5d8641238b2df202d9c4378951d
kevin.jacobson <kevin.jacobson@6a49e648-2030-3c6c-6fd1-c29bfb55b302>
2010-08-20 03:01:00 +0000

    # 1.13 Added more comments to source #      Added logic to prevent duplicate entries from being added to the CSV file #      Added feature to log outgoing calls to CSV

--- src/sip2yac.pl
+++ src/sip2yac.pl
@@ -37,6 +37,9 @@
 #      Format: 2125556789,Capt. Crunch
 # 1.12 Add additional reverse lookup sources
 #      Rename project to sip2yac
+# 1.13 Added more comments to source
+#      Added logic to prevent duplicate entries from being added to the CSV file
+#      Added feature to log outgoing calls to CSV
 
 
 use IO::Socket;
@@ -60,7 +63,7 @@ autoflush STDOUT 1;
 ######################################################################
 #########   User Defined Variables                           #########
 ######################################################################
-my ($pid, $child, $all_devs,$notifylist, $ignore, $debugmsg, @hosts_to_notify);
+my ($pid, $child, $all_devs, $notifylist, $debugmsg, @hosts_to_notify);
 my $debug=0;
 my $debug2=0;
 my $quiet=0;
@@ -70,6 +73,9 @@ my $user="root";
 my $promisc=1;
 my $to_ms=1000;
 my $useEvtlog=0;
+my $log2csv=0;
+my $update_csv=0;
+my $ignore="";
 my $ident="sip2yac";
 my $facility="local1";
 my $options="pid,perror";
@@ -89,6 +95,13 @@ $dbh->{csv_tables}{sip2yac} = {
         quote_char  => undef,
         escape_char => undef,
         col_names => [qw( cidnum cidname )],
+        };
+$dbh->{csv_tables}{CallLog} = {
+        eol         => "\n",
+        sep_char    => ",",
+        quote_char  => undef,
+        escape_char => undef,
+        col_names => [qw( date phonenum phonename )],
         };
 $PerlSvc::Config{ServiceName} = "sip2yac";
 $PerlSvc::Config{DisplayName} = "sip2yac";
@@ -114,9 +127,13 @@ my $snaplen=50000;
 # End child global variables
 ######################################################################
 
-#my $ver = "1.11";
 print "sip2yac $version (win32 impl. by Kevin Jacobson)\n";
 
+### All PerlSvc:: calls are specific to the ActiveState Perl PDK
+
+#
+# Tell the service what to do when issued a 'Start' command
+#
 sub PerlSvc::Startup() {
 	&getOptions(@ARGV) || die "Could not get options\n";
 	$useEvtlog=1;
@@ -132,6 +149,9 @@ sub PerlSvc::Startup() {
 	&ParentLeaveNow();
 }
 
+#
+# Tell the service what to do when running from the command-line
+#
 sub PerlSvc::Interactive() {
 	&getOptions(@ARGV) || die "Could not get options\n";
 	&open_evtlog();
@@ -139,17 +159,25 @@ sub PerlSvc::Interactive() {
 	exit;
 }
 
+#
+# Tell the service what to do when issued a '--install' command
+#
 sub PerlSvc::Install() {
 	if(scalar(@ARGV) > 0 && $ARGV[0]=~/-C=(.*)/){
 			$PerlSvc::Config{Parameters} = $ARGV[0];
 	}
 }
 
-
+#
+# Get the calling thread's last error code (return value)
+#
 sub ErrorReport {
 	log_msg(Win32::GetLastError());
 }
 
+#
+# Kill spawned process(es)
+#
 sub kill_child() {
     if (defined($child))
     {
@@ -160,12 +188,18 @@ sub kill_child() {
     }
 }
 
+#
+# Stub to handle cleanup when stopping the service
+#
 sub ParentLeaveNow() {
     kill_child();
     log_msg("Parent exiting...");
     exit;
 }
 
+#
+# Launching child process from the service
+#
 sub launch_proc() {
 	if (!defined($child)) {
 		Win32::Process::Create($child,
@@ -179,12 +213,18 @@ sub launch_proc() {
 	}
 }
 
+#
+# Init windows event log for use
+#
 sub open_evtlog() {
     if ($useEvtlog == 1) {
         Sys::Syslog::openlog $ident, $options, $facility || die "Couldn't open Event Log";
     }
 }
 
+#
+# Send message to event log if enabled, else console
+#
 sub log_msg($) {
     my ($msg) = @_;
 
@@ -196,23 +236,33 @@ sub log_msg($) {
     }
 }
 
+#
+# Main sub
+#
 sub start_AS {
 	log_msg("Starting up...");
 	log_msg("Device: $dev") if (defined($dev) && !($quiet));
 	log_msg("WinPcap Filter: $filter_str") if (defined($filter_str) && !($quiet));
 	log_msg("Number to ignore: $ignore") if (defined($ignore) && !($quiet));
 	log_msg("YAC listeners: $notifylist") if (defined($notifylist) && !($quiet));
+  # Create CSV file if it doesn't exist
   if (! -e "./sip2yac.csv") {
     $dbh->do ("CREATE TABLE sip2yac (cidnum CHAR (10), cidname CHAR (128))");
   }
+  if (! -e "./CallLog.csv" && $log2csv) {
+    $dbh->do ("CREATE TABLE CallLog (date CHAR (24), phonenum CHAR (10), phonename CHAR(128))");
+  }
+  # Test if any network connections exist
 	my $pcap_test = Net::Pcap::lookupdev(\$err);
 	if (defined $err) { die "\n\nUnable to find any network device.  Make sure WinPcap is installed.\n\n"; }
 	
+  # Loop thru network interfaces to provide a list to the user
 	dev: foreach $_ (Net::Pcap::findalldevs(\$err)) {
 		next dev if $_ =~ /Generic/;
 		my $net = '';
 		my $mask = '';
 		my $try_this = '';
+    # Only select interfaces that are on a private network
 		Net::Pcap::lookupnet($_, \$net, \$mask, \$err);
 		if (   (167772160 <= $net && $net <= 184549375)
 				|| (2886729728 <= $net && $net <= 2887778303)
@@ -220,6 +270,8 @@ sub start_AS {
 			) { $try_this = " *" };
 		$all_devs .= "\n".$_.$try_this;
 	}
+  
+  # Attempt to connect to network interface with lipPcap
 	$pcap_t=Net::Pcap::open_live($dev, $snaplen, $promisc, $to_ms, \$err) || die "Error opening libpcap: $err\n\nUse one of the following interfaces (try those with a * first):\n".$all_devs."\n"; #start sniffing
 	if ($filter_str ne "") {
 		my $filter_t;
@@ -230,7 +282,9 @@ sub start_AS {
 	return;
 }
 
-#Process the packet
+#
+# libPcap packet processing
+#
 sub process_pkt {
 	autoflush STDOUT 1;
 
@@ -243,29 +297,34 @@ sub process_pkt {
 		log_msg("not ok");
 		exit;
 	}
-	#get datetimestamp of packet
+	# get datetimestamp of packet
 	my ($sec, $min, $hour, $mday, $mon, $year)=localtime($hdr->{tv_sec});
 	$year+=1900;
 	$mon+=1;    
-	my $datestamp="$mon/$mday $hour:$min";
+	my $datestamp="$year-$mon-$mday $hour:$min:$sec";
 
-	#Strip Ethernet portion of packet off
+	# Strip Ethernet portion of packet off
 	my $ip_obj=NetPacket::IP->decode(eth_strip($pkt));
 	my $proto=$ip_obj->{proto};
 	my ($tcp_obj, $udp_obj, $flags, $dataset) = "";
 
+  # Only prepare UDP traffic since normal SIP traffic is UDP
+  # http://www.iana.org/assignments/protocol-numbers/protocol-numbers.xml
 	if($proto==17){
 		$udp_obj=NetPacket::UDP->decode(ip_strip(eth_strip($pkt)));
 		$dataset=$udp_obj->{data};
 	} elsif ($debug2) {
 		$udp_obj=NetPacket::UDP->decode(ip_strip(eth_strip($pkt)));
 		$dataset=$udp_obj->{data};
-  }    
+  }
 
-	print("$dataset\n") if ($debug2);
+	# A UDP packet has arrived and has been prepared  
 	if (defined($dataset)) {
-    $dataset = "INVITE sip: From: \"\" <sip:8165894109@>" if ($debug2);
+    print("$dataset\n") if ($debug2);
+    $dataset = "INVITE sip: From: \"\" <sip:2125556789@>" if ($debug2);
+    # Check to see if UDP traffic was a SIP INVITE message and process
 		if ($dataset=~/INVITE sip\:/){
+      # Check to see if this is an INCOMING call
 			if ($dataset=~/From\:\s*"?(.*)"?\s*<sip\:(.*)@/ && $2 !~ /$ignore/) {
 				my $callername = $1;
 				my $callernumber = $2;
@@ -284,6 +343,7 @@ sub process_pkt {
             $station = $3;
         }
 				$callernumber=~s/^(\d{3})(\d{3})(\d{4})$/$1-$2-$3/;
+        # Do reverse name lookup if no name was sent by the provider
         if ($callername eq "") {
           log_msg("\nReceived no CNAM/CIDNAME from Telco! Attempting Reverse Lookup...\n");
           my @reversel_providers = qw( csv_lookup anywho_lookup addresses_lookup google_lookup
@@ -297,12 +357,17 @@ sub process_pkt {
           }
         }
 				log_msg("\nDetected incoming call: $callername $callernumber\n");
+        # Format info for YAC
 				my $msg = "\@CALL$callername~$callernumber";
 				log_msg("YAC message to send: $msg\n");
+        
+        # Prep list of YAC listeners
 				@hosts_to_notify = $notifylist;
 				if ($notifylist =~ /\,/) {
 					@hosts_to_notify = split(/\,/, $notifylist);
 				}
+        
+        # Send message to defined YAC listeners
 				foreach my $host (@hosts_to_notify) {
 					my ($notifyaddr,$notifyport) = split(/\:/,$host);
 					if (defined($notifyaddr) && defined($notifyport)) {
@@ -318,12 +383,26 @@ sub process_pkt {
 					}
 				}
 			}
+      $dataset = "INVITE sip: To: <sip:6166989392@>" if ($debug2);
+      # Check to see if this is an OUTGOING call
+      # Only process if OUTGOING call logging is enabled 
+      if ($dataset=~/To\:\s*<sip\:(.*)@/ && $1 !~ /$ignore/ && $log2csv) {
+        my $callernumber = $1;
+				$callernumber=~s/^\s+//g;
+				$callernumber=~s/\s+$//g;
+				$callernumber=~s/^1//g;
+        log_msg("\nDetected outgoing call: $callernumber\n");
+        &logCall($datestamp, $callernumber);
+      }
 		}
 	}
   exit if ($debug2);
 	return;
 }
 
+#
+# Set program options via config file or command line arguments
+#
 sub getOptions{
     my(@ARGV)=@_;
     my $arg;
@@ -337,6 +416,7 @@ sub getOptions{
                $dev=$1 if($arg=~/dev=(.*)/);
                $filter_str=$1 if($arg=~/filter='(.*)'/);
                $update_csv=1 if ($arg=~/updatecsv=1/);
+               $log2csv=1 if ($arg=~/logoutgoing=1/);
                $promisc=1 if ($arg=~/promisc=1/);
                $notifylist=$1 if ($arg=~/y=(.*)/);
                $to_ms=$1 if ($arg=~/timeout=(.*)/);
@@ -354,6 +434,7 @@ sub getOptions{
             $filter_str=$1 if($arg=~/-f=(.*)/);
             $ignore=$1 if($arg=~/-i=(.*)/);
             $update_csv=1 if ($arg=~/-u/);
+            $log2csv=1 if ($arg=~/-o/);
             $promisc=1 if ($arg=~/-p/);
             $notifylist=$1 if ($arg=~/-y=(.*)/);
             $to_ms=$1 if ($arg=~/-to=(.*)/);
@@ -366,6 +447,7 @@ sub getOptions{
                 print "  -d=<dev>    Specify the device from which packets are captured\n";
                 print "  -i=<num>    Specify a phone number to ignore (yours)\n";
                 print "  -u          Write any reverse lookup to local CSV\n";
+                print "  -o          Log outgoing calls to local CSV\n";
                 print "  -f=\"filter\" String to filter on enclosed in single quotes\n";
                 print "              (DEFAULT: \"udp and port 5060\")\n";
                 print "  -y=ip:port  IP address(es) and port to send YAC messages to (comma separated)\n";
@@ -381,17 +463,24 @@ sub getOptions{
         }
     }
 	$filter_str =~ s/\'|\"//g;
-    return 1;
+  return 1;
 }
 
 
 sub anywho_lookup {
+  # assign arguments
 	my ($npa, $nxx, $station) = @_;
+  # creste http object with timeout
 	my $ua = LWP::UserAgent->new( timeout => 45);
+  # create url with phone number
 	my $URL = 'http://whitepages.anywho.com/results.php';
+  # set browser useragent to try and look like a real browser
 	$URL .= qq(?qnpa=$npa&qnpanxx=$npa$nxx&qnxx=$nxx&qp=$nxx$station&qstation=$station);
+  # make the connection
 	$ua->agent($useragent);
+  # store the resulting content
 	my $req = new HTTP::Request GET => $URL;
+  # process content if we actually got something
 	my $res = $ua->request($req);
 	if ($res->is_success()) {
 		if ($res->content =~ /Find More Information for (.*)<\/a>/) {
@@ -403,12 +492,19 @@ sub anywho_lookup {
 }
 
 sub google_lookup {
+  # assign arguments
 	my ($npa, $nxx, $station) = @_;
+  # creste http object with timeout
 	my $ua = LWP::UserAgent->new( timeout => 45);
+  # create url with phone number
 	my $URL = qq(http://www.google.com/search?rls=en&q=phonebook:$npa$nxx$station&ie=UTF-8&oe=UTF-8);
+  # set browser useragent to try and look like a real browser
 	$ua->agent($useragent);
+  # make the connection
 	my $req = new HTTP::Request GET => $URL;
+  # store the resulting content
 	my $res = $ua->request($req);
+  # process content if we actually got something
 	if ($res->is_success()) {
 		if ($res->content =~ /<td>(.+)<td>\(<b>$npa/) {
 			my $clidname = $1;
@@ -419,12 +515,19 @@ sub google_lookup {
 }
 
 sub whitepages_lookup {
+  # assign arguments
 	my ($npa, $nxx, $station) = @_;
+  # creste http object with timeout
 	my $ua = LWP::UserAgent->new( timeout => 45);
+  # create url with phone number
 	my $URL = qq(http://api.whitepages.com/reverse_phone/1.0/?phone=$npa$nxx$station;api_key=ebbe5c21ae226fdb7d26e1faef45b31e);
+  # set browser useragent to try and look like a real browser
 	$ua->agent($useragent);
+  # make the connection
 	my $req = new HTTP::Request GET => $URL;
+  # store the resulting content
 	my $res = $ua->request($req);
+  # process content if we actually got something
 	if ($res->is_success()) {
     if ($res->content =~ /<wp:displayname>(.*)<\/wp:displayname>/) {
       $clidname = $1;
@@ -439,12 +542,19 @@ sub whitepages_lookup {
 }
 
 sub addresses_lookup {
+  # assign arguments
 	my ($npa, $nxx, $station) = @_;
+  # creste http object with timeout
 	my $ua = LWP::UserAgent->new( timeout => 45);
+  # create url with phone number
 	my $URL = qq(http://phonenumbers.addresses.com/results.php?ReportType=33&qfilter[pro]=on&qi=0&qk=10&qnpa=$npa&qp=$nxx$station);
+  # set browser useragent to try and look like a real browser
 	$ua->agent($useragent);
+  # make the connection
 	my $req = new HTTP::Request GET => $URL;
+  # store the resulting content
 	my $res = $ua->request($req);
+  # process content if we actually got something
 	if ($res->is_success()) {
 		if ($res->content =~ /<a.*class=\"wp_listing_name\">(.*)<\/a>/) {
 			my $clidname = $1;
@@ -455,12 +565,19 @@ sub addresses_lookup {
 }
 
 sub yp_lookup {
+  # assign arguments
 	my ($npa, $nxx, $station) = @_;
+  # creste http object with timeout
 	my $ua = LWP::UserAgent->new( timeout => 45);
+  # create url with phone number
 	my $URL = qq(http://yellowpages.addresses.com/yellow-pages/phone:$npa$nxx$station/listings.html);
+  # set browser useragent to try and look like a real browser
 	$ua->agent($useragent);
+  # make the connection
 	my $req = new HTTP::Request GET => $URL;
+  # store the resulting content
 	my $res = $ua->request($req);
+  # process content if we actually got something
 	if ($res->is_success()) {
 		if ($res->content =~ /<a class=\"listing_name\".*>(.*)<\/a>/) {
 			my $clidname = $1;
@@ -471,12 +588,19 @@ sub yp_lookup {
 }
 
 sub telcod_lookup {
+  # assign arguments
 	my ($npa, $nxx, $station) = @_;
+  # creste http object with timeout
 	my $ua = LWP::UserAgent->new( timeout => 45);
+  # create url with phone number
 	my $URL = qq(http://www.telcodata.us/query/queryexchangexml.html?npa=$npa&nxx=$nxx);
+  # set browser useragent to try and look like a real browser
 	$ua->agent($useragent);
+  # make the connection
 	my $req = new HTTP::Request GET => $URL;
+  # store the resulting content
 	my $res = $ua->request($req);
+  # process content if we actually got something
 	if ($res->is_success()) {
 		if ($res->content =~ /<valid>(.*)<\/valid>/) {
 			my $clidname = $1;
@@ -495,10 +619,13 @@ sub telcod_lookup {
 }
 
 sub csv_lookup {
+  # assign arguments
 	my ($npa, $nxx, $station) = @_;
+  # sql query to csv file
   my $sth = $dbh->prepare (qq;SELECT cidname FROM sip2yac WHERE cidnum = '$npa$nxx$station';);
   $sth->execute();
-  my ($cidname);
+  # setup column binding
+  my $cidname;
   $sth->bind_columns (undef, \$cidname);
   while ($sth->fetch) {
     return $cidname;
@@ -508,8 +635,50 @@ sub csv_lookup {
 }
 
 sub csv_update {
+  # assign arguments
 	my ($cidnum, $cidname) = @_;
+  # check to make sure we have a name to save with the number
   if ($cidname ne "") {
-    $dbh->do (qq;INSERT INTO sip2yac VALUES('$cidnum','$cidname'););
+    # local variables to split the number into
+    my ($npa, $nxx, $station) = "";
+    if ($cidnum =~ /^(\d{3})(\d{3})(\d{4})$/) {
+      # assign parts of the number if they match the pattern
+      $npa = $1;
+      $nxx = $2;
+      $station = $3;
+    }
+    # check the csv to see if this number has already been stored
+    if (&csv_lookup($npa, $nxx, $station) eq "") {
+      # add name and number if the csv lookup returned nothing
+      $dbh->do (qq;INSERT INTO sip2yac VALUES('$cidnum','$cidname'););
+    }
   }
+}
+
+sub logCall {
+  # assign arguments
+	my ($date, $phonenum) = @_;
+  # vars for phone number split
+  my ($npa, $nxx, $station) = "";
+  if ($phonenum =~ /^(\d{3})(\d{3})(\d{4})$/) {
+    # assign parts of the number if they match the pattern
+    $npa = $1;
+    $nxx = $2;
+    $station = $3;
+  }
+  
+  # lookup who is being called
+  my $phonename = "";
+  my @reversel_providers = qw( csv_lookup anywho_lookup addresses_lookup google_lookup
+                               yp_lookup whitepages_lookup telcod_lookup
+                              );
+  foreach $provider (@reversel_providers) {
+    last if $phonename ne "";
+    print("\tTrying $provider...\n") if ($debug);
+    $phonename = &$provider($npa, $nxx, $station);
+    &csv_update ($npa.$nxx.$station, $phonename) if ($update_csv);
+  }
+  
+  # Log call reguardless of whether we have a name
+  $dbh->do (qq;INSERT INTO CallLog VALUES('$date','$phonenum','$phonename'););
 }
